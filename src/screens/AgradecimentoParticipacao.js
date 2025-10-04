1a84004337ec4d55469d70587337858ed22630fc
Samuel Ribeiro <samcos@alunos.utfpr.edu.br>
2025-10-04 10:56:06 -0300

    styles global

---
+++ src/screens/AgradecimentoParticipacao.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const AgradecimentoParticipacao = () =>{
+    return(
+        <View>
+            <Text>Obrigado por participar da pesquisa!</Text>
+            <Text>Aguardamos você no próximo ano!</Text>
+        </View>
+    )
+}
+
+export default AgradecimentoParticipacao
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#372775',
+    padding: 16,
+  },
+  text: {
+    fontSize: 16,
+    color: '#ffffffff',
+  },
+  header: {
+    backgroundColor: '#2B1D62',
+    color: '#FFFFFF'
+  },
+})
