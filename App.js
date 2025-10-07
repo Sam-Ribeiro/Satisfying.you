a59a16c023b79d15d503e42964e6aadb59e61301
Samuel Ribeiro <samcos@alunos.utfpr.edu.br>
2025-10-06 21:18:09 -0300

    appjs sam

--- App.js
+++ App.js
@@ -1,33 +1,61 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Coleta from "./src/screens/Coleta";
-import AgradecimentoParticipacao from "./src/screens/AgradecimentoParticipacao";
-import AcoesPesquisa from "./src/screens/AcoesPesquisa";
-import Relatorio from "./src/screens/Relatorio";
-const Stack = createStackNavigator()
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator 
-                initialRouteName="Carnaval"
-                screenOptions={{
-                    headerStyle: {
-                    backgroundColor: '#2B1D62', 
-                    height: 50
-                },
-                    headerTintColor: '#fff', 
-                    headerTitleStyle: {
-                    fontSize: 22,
-                },
-            }}>
-                <Stack.Screen name="Carnaval" component={AcoesPesquisa} options={{headerShown:true}}/>
-                <Stack.Screen name="Relatorio" component={Relatorio} options={{headerShown:true}}/>
-                <Stack.Screen name="Coleta" component={Coleta} options={{headerShown:false}}/>
-                <Stack.Screen name="AgradecimentoParticipacao" component={AgradecimentoParticipacao} options={{headerShown:false}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+import Login from './src/screens/Login';
+import Home from './src/screens/Home';
+import AcoesPesquisa from './src/screens/AcoesPesquisa';
+import Coleta from './src/screens/Coleta';
+import Relatorio from './src/screens/Relatorio';
+import RecuperarSenha from './src/screens/RecuperarSenha';
+import Register from './src/screens/Register';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {backgroundColor: '#2B1D62'},
+          headerTintColor: '#fff',
+        }}>
+        {/* */}
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+
+        {/* */}
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+
+        {/*  */}
+        <Stack.Screen
+          name="AcoesPesquisa"
+          component={AcoesPesquisa}
+          options={{title: 'Ações de Pesquisa'}}
+        />
+        <Stack.Screen name="Coleta" component={Coleta} />
+        <Stack.Screen name="Relatorio" component={Relatorio} />
+
+        {/*  */}
+        <Stack.Screen
+          name="RecuperarSenha"
+          component={RecuperarSenha}
+          options={{title: 'Recuperar Senha'}}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{title: 'Cadastrar'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default App
